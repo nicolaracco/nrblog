8eaa84eb1adf67ae711a5c066d5fb69c3c19cae7
Nicola Racco <nicola.racco@gmail.com>
2010-05-21 18:21:05 +0200

    Added meta description for contents

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,4 +1,14 @@
 module LayoutHelper
+  def meta_description(meta_description)
+    @_content_for[:meta_description] = meta_description
+    @show_description = true
+  end
+
+  def meta_description?
+    @show_description
+  end
+
+
   def title(page_title, show_title=true)
     @_content_for[:title] = page_title.to_s
     @show_title = show_title
