b64b34271d6fda4220e3afbd05d5174bd3adf960
Nicola Racco <nicola.racco@gmail.com>
2010-05-25 11:23:59 +0200

    Cleanup

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -7,13 +7,4 @@ class Mailer < ActionMailer::Base
     @message = contact.body
     mail :to => recipient, :subject => "[#{t :sitename}] #{contact.subject}"
   end
-
-  def comment_notify(comment, recipient, content_url)
-    @sender_name = comment.user_name
-    @sender_email = comment.user_email
-    @content = comment.content
-    @message = comment.body
-    @content_url = content_url
-    mail :to => recipient, :subject => "[#{t :sitename}] #{t(:subject, :scope => [:mailer, :comment_notify], :content => comment.content.title)}"
-  end
 end
