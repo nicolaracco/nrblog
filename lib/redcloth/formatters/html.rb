eedb070533afaaac5206b1d27bd410d7a4b2b912
Nicola Racco <nicola.racco@gmail.com>
2010-05-30 16:43:22 +0200

    Added ignore feature for CodeRay integration

--- lib/redcloth/formatters/html.rb
+++ lib/redcloth/formatters/html.rb
@@ -28,20 +28,22 @@ module RedCloth::Formatters::HTML
     if (classes.include? 'ish')
       super
     else
-      s_code = CGI.unescapeHTML opts[:text]
       code_type = opts[:lang]
-      if code_type.nil? && s_code.index(/\[\w+\]/) == 0 #Fix error for lang selector broken from 4.2.0
-        cb_end = s_code.index(']')
-        code_type = s_code[1, cb_end - 1]
-        s_code = s_code[cb_end + 1, s_code.length - cb_end - 1]
+      if code_type.nil? && opts[:text].index(/\[\w+\]/) == 0 #Fix error for lang selector broken from 4.2.0
+        cb_end = opts[:text].index(']')
+        code_type = opts[:text][1, cb_end - 1]
+        opts[:text] = opts[:text][cb_end + 1, s_code.length - cb_end - 1]
       end
       code_type ||= 'plain'
-
-      s_code = s_code.gsub /^\s*\\\s*$/, '' #Remove blank line markers
-      scanned = CodeRay.scan(s_code, code_type.to_sym)
-      lines_count = 0
-      s_code.each_line { |line| lines_count += 1 }
-      @isblock ? scanned.div(:line_numbers => lines_count > 1 ? :table : :inline, :css => :class) : scanned.span(:css => :class)
+      if (code_type == 'none')
+        super
+      else
+        s_code = CGI.unescapeHTML(opts[:text]).gsub /^\s*\\\s*$/, '' #Remove blank line markers
+        scanned = CodeRay.scan(s_code, code_type.to_sym)
+        lines_count = 0
+        s_code.each_line { |line| lines_count += 1 }
+        @isblock ? scanned.div(:line_numbers => lines_count > 1 ? :table : :inline, :css => :class) : scanned.span(:css => :class)
+      end
     end
   end
 end
