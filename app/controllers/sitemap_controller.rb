43fb6832ca0faeeebe8eee81bc0eed9d6f9d3de1
Nicola Racco <nicola.racco@gmail.com>
2010-06-01 16:02:10 +0200

    Sitemap now sends also rss feeds

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,6 +3,8 @@ class SitemapController < ApplicationController
 
   def index
     @posts = Content.find(:all, :conditions => 'published = 1', :order => 'updated_at DESC', :limit => 50000)
+    @categories = Category.find(:all)
+    @tags = Tag.find(:all)
     render :layout => false
   end
 end
