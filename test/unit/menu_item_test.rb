7247b90db78c83c519c30eff5219410d3297fa58
Nicola Racco <nicola.racco@gmail.com>
2010-06-07 08:22:50 +0200

    Added some tests

--- test/unit/menu_item_test.rb
+++ test/unit/menu_item_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class MenuItemTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "Cannot save menu_item without required fields" do
+    mi = MenuItem.new
+    assert !mi.save
+  end
+  test "Generate title and order on save" do
+    mi = MenuItem.new({:title_it => 'Due menu', :url => 'testaddress', :menu_type => 'horizontal_top' })
+    assert mi.save
   end
 end
